f4a45287665eb111aa2798953551f9f87cf6e5fb
Lucas Calabria <lucascalabria86@gmail.com>
2023-04-04 16:00:48 -0300

    first page

--- My-Twitter-FrontEnd/src/components/PhoneHeader.jsx
+++ My-Twitter-FrontEnd/src/components/PhoneHeader.jsx
@@ -1,33 +1,12 @@
-import styled from 'styled-components'
 import wifi from '../assets/wifi.svg'
 import signal from '../assets/signal.svg'
 import battery from '../assets/battery.svg'
+import { GlobalPhoneHeader, Hour, PhoneStatus } from '../styles/PhoneHeaderStyles'
 
 export function PhoneHeader () {
-    const GlobalPhoneHeader = styled.div`
-    width: 100%;
-    height: 2.75rem;
-    background-color: blue;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding-left: 3.75rem;
-    padding-right: 2.8125rem;
-    `
-
-    const Hour = styled.p `
-    color: black;
-    font-weight: bold;
-    font-size: 0.75rem;
-    `
-
-    const PhoneStatus = styled.div `
-    width: 2rem;
-    display: flex;
-    `
     return (
         <GlobalPhoneHeader>
-        <Hour>Hora</Hour>
+        <Hour>9:41</Hour>
 
         <PhoneStatus>
             <img src={signal} alt='signal-bar' />
--- My-Twitter-FrontEnd/src/pages/LoginPage.jsx
+++ My-Twitter-FrontEnd/src/pages/LoginPage.jsx
@@ -1,7 +1,99 @@
+import styled from 'styled-components'
+import logoicon from '../assets/logoicon.svg'
+import GoButton from '../assets/gradientbutton.svg'
+import LoginLine from '../assets/loginline.svg'
+
 export function LoginPage () {
+
+    const GlobalHeader = styled.div `
+    min-height: 94.8vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    `
+
+    const Logo = styled.img `
+    padding-bottom:0.6875rem;
+    `
+
+    const Title = styled.h2 `
+    font-size: 2.25rem ;
+    font-weight: 700;
+    font-family: 'IBM Plex Sans', sans-serif;
+    line-height: 2.925rem;
+    `
+
+    const SubTitle = styled.p `
+    font-family: 'IBM Plex Sans', sans-serif ;
+    font-size: 1rem;
+    line-height: 1.3rem;
+    `
+
+    const InputArea = styled.div `
+    width: 100%;
+    height:7.5rem ;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 0.5rem;
+    margin-top: 6.6875rem ;
+    `
+
+    const InputLogin = styled.input `
+    width: 22.6875rem ;
+    height: 3.75rem;
+    border-radius: 0.25rem ;
+    border: 1px solid #d5d8de;
+    padding-left: 1rem;
+
+    ::placeholder {
+        color: #323941 ;
+        font-family: 'Noto Sans';
+    }
+    `
+
+const ButtonArea = styled.div `
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+`
+
+const GradientButton = styled.img `
+    width: 22.6875rem;
+    height: 3.1875rem;
+    margin-top: 3.5rem ;
+`
+
+const SignUpButton = styled.button `
+    width: 22.6875rem;
+    height: 3.1875rem;
+    background-color: transparent;
+    border: 2px solid #FE7E02;
+    color: #FE7E02 ;
+    font-family: 'Noto Sans';
+    border-radius: 1.6875rem;
+
+`
     return (
-        <>
-        <h1>Login</h1>
-        </>
+        <GlobalHeader>
+        <Logo src={logoicon} alt='logoicon' />
+        <Title>MyTwitter</Title>
+        <SubTitle>Rede Social da Labenu</SubTitle>
+        <InputArea>
+        <InputLogin placeholder='Email' />
+        <InputLogin placeholder='Senha' />
+        </InputArea>
+        <ButtonArea>
+        <GradientButton src={GoButton} alt='Go Button' />
+        <img src={LoginLine} />
+        <SignUpButton >Criar Conta</SignUpButton>
+        </ButtonArea>
+        </GlobalHeader>
     )
 }
